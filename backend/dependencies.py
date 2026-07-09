423d8a56017bc1e6b776b8420993f109fbe33554
aridontknowcode <m.harith.hadi@gmail.com>
2026-07-09 23:01:02 +0800

    Evak

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -1,6 +1,7 @@
 # dependencies.py
 import torch
 from models_def import RADDINOClassifier, DEVICE
+from pathlib import Path
 
 
 class ModelContainer:
@@ -13,8 +14,15 @@ class ModelContainer:
         if self.tb_classifier is None:
             print("Loading TB classifier once...")
             self.tb_classifier = RADDINOClassifier().to(DEVICE)
+
+            current_dir = Path(__file__).parent.resolve()
+            model_path = current_dir / "models" / "tb_classifier (5).pt"
+            
+            # Debugging print
+            print(f"Looking for model at: {model_path}")
+
             # Add weights_only=False if you are loading local .pt files
-            state_dict = torch.load("models/tb_classifier (5).pt", map_location=DEVICE, weights_only=False)
+            state_dict = torch.load(model_path, map_location=DEVICE, weights_only=False)
             self.tb_classifier.load_state_dict(state_dict, strict=False)
             self.tb_classifier.eval()
         return self.tb_classifier
